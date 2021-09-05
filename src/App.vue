9f723d499a81d981e52c3836e5a95832818b730b
xxlfiren1709 <xxlfiren1709@gmail.com>
2021-09-05 13:40:30 +0200

    mobile fix

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,7 @@
 
   <v-app id="lars-homepage">
     <side-navigation></side-navigation>
-    <v-main>
-      <!--           
-      <v-img  class="parallax pt-5" height="50vh" contain  src="/profilfoto.jpg" >
-        <h1 class="text-h1 ml-6 black-text">Lars <br> Ulbricht</h1>
-      </v-img>-->
-
-      <v-card class="ml-0 mt-0">
-        <v-img class="parallax pt-5" height="100vh">
-          <h1
-            id="black-text"
-            :class="{ 'text-center': this.$vuetify.breakpoint.xs }"
-            class="text-h1 mt-10"
-          >
-            Lars <br v-if="!this.$vuetify.breakpoint.xs" />
-            Ulbricht
-          </h1>
-          <div v-if="this.scrolled == 0" id="down_arrow">
-            <v-icon
-              @click="$vuetify.goTo('#about_head')"
-              class="mt-30 chevron bottom"
-              color="black"
-              size="6rem"
-              >mdi-chevron-down</v-icon
-            >
-          </div>
-        </v-img>
-      </v-card>
-
-      <div id="lang_control" class="d-flex justify-end">
+     <div id="lang_control" class="d-flex justify-end">
         <v-btn
           :disabled="$vuetify.lang.current == 'ja'"
           elevation="2"
@@ -50,6 +22,33 @@
           >EN</v-btn
         >
       </div>
+    <v-main>
+      <!--           
+      <v-img  class="parallax pt-5" height="50vh" contain  src="/profilfoto.jpg" >
+        <h1 class="text-h1 ml-6 black-text">Lars <br> Ulbricht</h1>
+      </v-img>-->
+  
+      <v-card class="ml-0 mt-0">
+        <v-img class="parallax pt-5 " height="100vh" >
+          <h1
+            id="black-text"
+            :class="{ 'text-center': this.$vuetify.breakpoint.xs }"
+            class="text-h1 mt-sm-10 ml-sm-10 name-title"
+          >
+            Lars <br v-if="!this.$vuetify.breakpoint.xs" />
+            Ulbricht
+          </h1>
+          <div v-if="this.scrolled == 0" id="down_arrow">
+            <v-icon
+              @click="$vuetify.goTo('#about_head')"
+              class="chevron bottom"
+              color="black"
+              size="6rem"
+              >mdi-chevron-down</v-icon
+            >
+          </div>
+        </v-img>
+      </v-card>
       <v-banner class="mt-4" elevation="3" single-line id="about_head"
         ><v-icon class="mr-4">mdi-message</v-icon> Motto
       </v-banner>
@@ -59,7 +58,7 @@
           'text-center': this.$vuetify.breakpoint.xs,
           'pl-13': this.$vuetify.breakpoint.sm,
         }"
-        class="mt-4 mb-4"
+        class="mt-4 mb-4 ml-sm-10"
         id="about_me_text"
       >
         <v-icon  :class="{
@@ -73,6 +72,7 @@
           'mr-2': this.$vuetify.breakpoint.sm,
         }" class="pb-8 quote-icon">mdi-format-quote-close</v-icon>
       </v-card-text>
+  
 
       <about-me></about-me>
 
